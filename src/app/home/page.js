1c283e9d77181fd2b81d69f115f2181184591cbd
ditoow <ghifariwiraa@gmail.com>
2024-11-24 18:52:03 +0700

    main

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "@/components/navbar";
 import { Button } from "@nextui-org/react";
+import "../globals.css";
 import ModalSelectMenu from "@/components/ModalSelectMenu";
 
 export default function HomePage() {
@@ -9,12 +10,12 @@ export default function HomePage() {
       <Navbar />
       <div className="flex items-center justify-between p-4">
         <div className="flex-1">
-          <img src="/images/belajar.png" alt="Logo" className="w-96 h-96 mx-auto" />
+          <img src="/images/belajar2.png" alt="Logo" className="w-96 h-96 mx-auto custom-image " />
         </div>
         <div className="flex-1 pl-8">
           <h1 className="text-3xl font-bold">QUIKA</h1>
-          <p className="mt-4 text-lg">Quiz Fisika Kelompok Dito !</p>
-          <Button>Click Me</Button>
+          <p className="mt-4 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
         </div>
       </div>
     </main>
