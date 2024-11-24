0d76d45f1759b6ea97f08a998341824d833edd63
ditoow <ghifariwiraa@gmail.com>
2024-11-24 13:21:02 +0700

    main

---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "@/components/navbar"
+import { Button } from "@nextui-org/react"
+import ModalSelectMenu from "@/components/ModalSelectMenu"
+export default function Page() {
+    return (
+        <main className="flex flex-col min-h-dvh">
+                <Navbar />
+        </main>
+    )
+}
