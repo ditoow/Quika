33491c4b338aad2c38e1fd84098d46649e43b3d7
ditoow <ghifariwiraa@gmail.com>
2024-11-24 19:14:54 +0700

    main

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -8,14 +8,20 @@ export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex items-center justify-between p-4">
+      <div className="flex items-center justify-center py-6">
         <div className="flex-1">
-          <img src="/images/belajar2.png" alt="Logo" className="w-96 h-96 mx-auto custom-image " />
+          <img
+            src="/images/belajar2.png"
+            alt="QUIKA logo"
+            className="mx-auto custom-image"
+          />
         </div>
         <div className="flex-1 pl-8">
           <h1 className="text-3xl font-bold">QUIKA</h1>
-          <p className="mt-4 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+          <p className="mt-4 text-lg">
+            Aplikasi belajar online yang membantu kamu untuk memahami materi
+            pelajaran dengan mudah dan menyenangkan.
+          </p>
         </div>
       </div>
     </main>
